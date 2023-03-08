8824e30d5355d85f0e8ae59283a8aa615cc24656
Cyclonicks <malo.philippe2021@gmail.com>
2023-03-08 11:03:23 -0500

    c11 ajout fonctions carrousel

--- carrousel.php
+++ carrousel.php
@@ -11,3 +11,14 @@
  * Plugin URI: https://github.com/cyclonicks/carrousel
  * Description: permet d'Afficher dans une boîte modale les images d'une galerie avec un système de navigation
  */
+
+wp_enqueue_style(   'em_plugin_carrousel_css',
+                plugin_dir_url(__FILE__) . "style.css",
+                array(),
+                $version_css);
+
+wp_enqueue_script(  'em_plugin_carrousel_js',
+                plugin_dir_url(__FILE__) ."js/carrousel.js",
+                array(),
+                $version_js,
+                true);
