f9a00370dc979a5d9cb5675764ccf8cd22ba8857
Cyclonicks <malo.philippe2021@gmail.com>
2023-03-08 10:51:33 -0500

    c11 début du plugin carrousel

---
+++ carrousel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Extension carrousel permet d'Afficher dans une boîte modale les images d'une galerie
+ * Package Name: Carrousel
+ * Version: 1.0.0
+ */
+
+/**
+ * Plugin Name: Carrousel
+ * Author: Philippe Malo
+ * Plugin URI: https://github.com/cyclonicks/carrousel
+ * Description: permet d'Afficher dans une boîte modale les images d'une galerie avec un système de navigation
+ */
