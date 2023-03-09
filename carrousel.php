b95d638bfa9ec226d2cff961579ec8c357b78c42
Cyclonicks <malo.philippe2021@gmail.com>
2023-03-09 14:50:59 -0500

    c12 carrousel.php enqueue et génération du code HTML

--- carrousel.php
+++ carrousel.php
@@ -11,3 +11,37 @@
  * Plugin URI: https://github.com/cyclonicks/carrousel
  * Description: permet d'Afficher dans une boîte modale les images d'une galerie avec un système de navigation
  */
+
+    function carrousel_enqueue(){
+
+        $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+        $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+        wp_enqueue_style('em_plugin_carrousel_css',
+                        plugin_dir_url(__FILE__) . "style.css",
+                        array(),
+                        $version_css
+                        );
+
+        wp_enqueue_script('em_plugin_carrousel_js',
+                        plugin_dir_url(__FILE__) ."js/carrousel.js",
+                        array(),
+                        $version_js,
+                        true
+                        );
+    }
+
+    add_action('wp_enqueue_scripts', 'carrousel_enqueue');
+
+    function creation_carrousel(){
+        return '
+                <button class="bouton__ouvrir">Ouvrir</button>
+                <div class="carrousel">
+                    <button class="bouton__x">X</button>
+                    <figure class="carrousel__figure"></figure>
+                    <form class="carrousel__form"></form>
+                </div>
+                ';
+            }
+            
+            add_shortcode('carrousel', 'creation_carrousel');
